d5dfbc8d8d3154540c7351b5b64dd2dd474445f7
michaelcurrin <michael.currin@gmail.com>
2019-11-11 12:55:44 +0200

    docs: Update comments

--- printenv.py
+++ printenv.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
+"""
+Copied from PyCharm script at:
+    /Applications/PyCharm CE.app/Contents/bin/
 
-# Copied from PyCharm script at
-#   /Applications/PyCharm CE.app/Contents/bin/.
-# This is similar to
-#  env > vars.txt
+This script is similar to bash commands:
 
+    The env command can be used to show the environment, if no command is provided.
+        $ env > vars.txt
+
+    See /usr/bin/printenv utility or builtin.
+        $ printenv > vars.txt
+
+        $ printenv SHELL
+        /bin/bash
+
+        $ echo $SHELL
+        /bin/bash
+"""
 # Dumps environment variables into specified file.
 # Format: zero-separated "name=value" pairs in platform encoding.
 
